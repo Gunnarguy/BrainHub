71e46a3c8494dfe066b1d5932490f934381f2ee4
gunnarguy <gunnarguy@me.com>
2025-08-12 19:59:52 -0700

    Initial Commit

---
+++ BrainHub/BrainHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainHubApp.swift
+//  BrainHub
+//
+//  Created by Gunnar Hostetler on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BrainHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
